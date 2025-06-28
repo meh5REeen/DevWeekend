eff8cb290eb0354b469fcf511026351a186c0e18
meh5REeen <mehreens826@gmail.com>
2025-06-28 12:07:49 +0400

    created pages and routes

--- Project-3/client/src/App.jsx
+++ Project-3/client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import SignOut from "./pages/SignOut.jsx";
 export default function App() {
   return (
-    <div className= "text-red-400">
-      App Mehreen
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route path ="/" element = {<Home/>}/>
+        <Route path ="/sign-up" element = {<SignUp/>}/>
+        <Route path ="/sign-out" element = {<SignOut/>}/>
+        <Route path ="/about" element = {<About/>}/>
+        <Route path ="/profile" element = {<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
