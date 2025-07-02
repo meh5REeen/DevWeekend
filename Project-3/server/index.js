569636559f63db9352ed90ac6dbf834dadd4b3ec
meh5REeen <mehreens826@gmail.com>
2025-07-02 08:16:26 +0500

    db connection done

--- Project-3/server/index.js
+++ Project-3/server/index.js
@@ -1,8 +1,16 @@
 import express from "express";
-
+import mongoose from "mongoose"
+import dotenv from "dotenv";
+dotenv.config()
 const app = express();
 
+mongoose.connect(process.env.MONGO_STRING).then(
+    () => {
+        console.log("Connected to MONGO");
+}).catch((err) => {
+    console.log(err);
+})
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("Server running on port 3000");
 })
