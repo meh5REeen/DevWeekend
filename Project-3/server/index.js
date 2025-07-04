06e0b56fcbc71550e1f7dff255507bed17a7e817
meh5REeen <mehreens826@gmail.com>
2025-07-04 10:30:37 +0500

    middleware function for errors

--- Project-3/server/index.js
+++ Project-3/server/index.js
@@ -22,6 +22,16 @@ app.use(express.json())
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 
+app.use((err,req,res, next) => {
+    const statusCode =  err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    }
+    )
+});
 
 app.listen(process.env.PORT, ()=>{
     console.log("Server running on port 3000");
