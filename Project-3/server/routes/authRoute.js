5952b1dba5c8f77d2a510ccf7bf6922fd5529c4e
meh5REeen <mehreens826@gmail.com>
2025-07-05 17:00:09 +0500

    sign-in api done

--- Project-3/server/routes/authRoute.js
+++ Project-3/server/routes/authRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { signup } from "../controllers/authController.js";
+import { signin, signup } from "../controllers/authController.js";
 
 export const authRouter = express.Router();
 
 authRouter.post("/signup",signup)
+authRouter.post("/signin",signin)
